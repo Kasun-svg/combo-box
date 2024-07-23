d25547855ebbd8255a62322c04841232ab9335ce
Kasun <Kasun-svg>
2024-07-23 12:40:27 +0300

    FEAT :Project initiated

--- src/MainFormController.java
+++ src/MainFormController.java
@@ -5,23 +5,11 @@ import javafx.scene.control.TextField;
 public class MainFormController {
 
 
-    public TextField txtNum1;
-    public TextField txtResult;
-    public TextField txtNum2;
+    public TextField txtName;
+    public TextField txtAnswer;
+    public Button btnPrint;
 
-    public void addTwoNumbers(ActionEvent actionEvent) {
-
-        String num1 = txtNum1.getText();
-        String num2 = txtNum2.getText();
-        String result = String.valueOf(Integer.parseInt(num1) + Integer.parseInt(num2));
-        txtResult.setText(result);
-
-
-    }
-
-    public void btnClear(ActionEvent actionEvent) {
-        txtNum1.setText("");
-        txtNum2.setText("");
-        txtResult.setText("");
+    public void OnActionOnPrint(ActionEvent actionEvent) {
+        txtAnswer.setText(txtName.getText());
     }
 }
