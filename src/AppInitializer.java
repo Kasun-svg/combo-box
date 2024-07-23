d7599a63238cb19428f63b4d84b213309d16d8c7
Kasun <Kasun-svg>
2024-07-23 10:35:36 +0300

    FEAT :Project initiated

---
+++ src/AppInitializer.java
@@ -0,0 +1,34 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class AppInitializer extends Application {
+    public static void main (String[]args){
+       launch(args);
+
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //ui open code
+       // URL resource = getClass().getResource("MainForm.fxml");
+        //        Parent load = FXMLLoader.load(resource);
+        //        Scene scene = new Scene(load);
+        //        primaryStage.setScene(scene);
+        //        primaryStage.show();
+        //
+
+
+
+
+        primaryStage.setScene(new Scene(FXMLLoader.load( getClass().getResource("MainForm.fxml"))));
+        primaryStage.show();
+
+
+    }
+}
