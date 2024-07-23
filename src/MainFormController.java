61f634ed2f204253c829e51d4fd16a7143fc43c7
Kasun <Kasun-svg>
2024-07-23 14:39:06 +0300

    FEAT :Project initiated

--- src/MainFormController.java
+++ src/MainFormController.java
@@ -1,6 +1,7 @@
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 public class MainFormController {
 
@@ -8,8 +9,17 @@ public class MainFormController {
     public TextField txtName;
     public TextField txtAnswer;
     public Button btnPrint;
+    public TextField txtPrintName;
+    public TextField txtNameR;
+    public TextField txtPrintNameR;
 
-    public void OnActionOnPrint(ActionEvent actionEvent) {
-        txtAnswer.setText(txtName.getText());
+    public void onKeyPressedAcion(KeyEvent keyEvent) {
+        String value = txtName.getText();
+        txtPrintName.setText(value);
+    }
+
+    public void onKeyReleased(KeyEvent keyEvent) {
+        String value = txtNameR.getText();
+        txtPrintNameR.setText(value);
     }
 }
